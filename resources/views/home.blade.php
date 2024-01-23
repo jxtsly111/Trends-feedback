0c19feae9a5e14e6f7f15e5a8aa0aa105d13746c
Your Name <jxtsly@gmail.com>
2024-01-23 10:59:28 +0100

    logout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -101,7 +101,15 @@ https://templatemo.com/tm-582-tale-seo-agency
                         @if (Route::has('login'))
                         <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
                             @auth
-                                <a href="{{ url('/dashboard') }}" class="font-semibold text-gray-600 hover:text-gray-900 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
+                                <li  class="scroll-to-section">
+                                    <x-app-layout>
+   
+                                    </x-app-layout>
+                                </li>
+                               
+                           
+                                   
+                                    
                             @else
                               <li>  <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a></li>
         
